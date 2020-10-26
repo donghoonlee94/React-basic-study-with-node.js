8f15cac387ff5c4335645aed5301e7d2bdb097f0
donghoonlee94 <94donghoon.lee@gmail.com>
2020-10-26 22:15:33 +0900

    ✨ Install mongoose, Connect mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://donghoon:qwer1234@reactbasic.6uksf.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(`MongoDB Error ${err}`));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
