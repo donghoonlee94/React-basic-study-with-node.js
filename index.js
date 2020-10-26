800404823878cb7d688e58d1608e00efc6032b93
donghoonlee94 <94donghoon.lee@gmail.com>
2020-10-26 22:51:43 +0900

    ✨ Install bodyparser, Added register post

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParper = require("body-parser");
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParper.urlencoded({ extended: true }));
+
+//aplication/json
+app.use(bodyParper.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -18,6 +27,15 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
