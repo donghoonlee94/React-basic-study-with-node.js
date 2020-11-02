0a363c09ae373a7cade901c644c9a85878d5e641
donghoonlee94 <94donghoon.lee@gmail.com>
2020-11-02 21:17:34 +0900

    ✨ Add Concurrently & antd, Set proxy

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 const bodyParper = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
@@ -27,6 +27,10 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/api/hello", (req, res) => {
+  res.send("get hello");
 });
 
 app.post("/api/users/register", (req, res) => {
